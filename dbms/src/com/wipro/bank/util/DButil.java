04cc74f2f6f52e6d603db32049dcb1bd7f46ed38
pranesh003 <praneshkumar2375@gmail.com>
2026-02-10 11:03:14 +0530

    initial commit

---
+++ dbms/src/com/wipro/bank/util/DButil.java
@@ -0,0 +1,25 @@
+package com.wipro.bank.util;
+ import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+//import java.sql.Statement;
+public class DButil {
+		
+	public static Connection getDBConnection() {
+		try {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		String url="jdbc:oracle:thin:@localhost:1521:XE";
+		String user="SYSTEM";
+		String pass="84380";
+		Connection connection=DriverManager.getConnection(url,user,pass);
+		return connection;
+		
+	
+}
+		catch(ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			return null;
+}
+	
+}
+}
